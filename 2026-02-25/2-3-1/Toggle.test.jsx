f798c410ed8e8660fc0b1f8b5ad0c6a49c004d3a
dohaim918 <nzspace1121@gmail.com>
2026-02-25 23:28:58 +0900

    Post-submission updates (2026-02-25)

--- 2026-02-25/2-3-1/Toggle.test.jsx
+++ 2026-02-25/2-3-1/Toggle.test.jsx
@@ -7,23 +7,45 @@ describe("Toggle Button", () => {
     render(<Toggle />);
     expect(screen.getByText("OFF")).toBeInTheDocument();
   });
+  describe("상태가 'ON'일 때", () => {
+    test("클릭하면 'ON'", async () => {
+      render(<Toggle />);
+      const button = screen.getByRole("button");
 
-  test("클릭하면 'ON'", async () => {
-    render(<Toggle />);
-    const button = screen.getByRole("button");
+      await userEvent.click(button);
 
-    await userEvent.click(button);
+      expect(screen.getByText("ON")).toBeInTheDocument();
+    });
 
-    expect(screen.getByText("ON")).toBeInTheDocument();
+    test("'ON'일 경우 초록색", async () => {
+      render(<Toggle />);
+      const button = screen.getByRole("button");
+
+      await userEvent.click(button);
+
+      expect(screen.getByText("ON")).toHaveStyle("color:rgba(34, 197, 94, 0.9)");
+    });
   });
 
-  test("다시 클릭시 'Off'", async () => {
-    render(<Toggle />);
-    const button = screen.getByRole("button");
+  describe("상태가 'OFF'일 때", () => {
+    test("다시 클릭시 'OFF'", async () => {
+      render(<Toggle />);
+      const button = screen.getByRole("button");
 
-    await userEvent.click(button);
-    await userEvent.click(button);
+      await userEvent.click(button);
+      await userEvent.click(button);
 
-    expect(screen.getByText("OFF")).toBeInTheDocument();
+      expect(screen.getByText("OFF")).toBeInTheDocument();
+    });
+
+    test("'Off'일 경우 회색", async () => {
+      render(<Toggle />);
+      const button = screen.getByRole("button");
+
+      await userEvent.click(button);
+      await userEvent.click(button);
+
+      expect(screen.getByText("OFF")).toHaveStyle("color:rgba(107, 114, 128, 0.9)");
+    });
   });
 });
